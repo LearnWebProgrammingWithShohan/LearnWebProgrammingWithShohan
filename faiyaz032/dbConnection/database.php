1dfae451f068f3ca04b6d1a6a6354d1873d3212a
faiyaz032 <faiyazrahman03@gmail.com>
2020-04-01 14:54:16 +0100

    Added Construct Method and Pass the credentials as Parameter

--- faiyaz032/dbConnection/database.php
+++ faiyaz032/dbConnection/database.php
@@ -1,26 +1,31 @@
 <?php
 class Database{
 
-    private $hostName;
-    private $username;
-    private $password;
-    private $dbName;
+    private $hostName = '';
+    private $username ='';
+    private $password ='';
+    private $dbName = '';
+
+    public function __construct($hostName,$username,$password,$dbName)
+    {
+        $this->hostName = $hostName;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbName =   $dbName;
+    }
 
     public function connect()
     {
-        $hostName = $this->hostName = "localhost";
-        $username = $this->username = "root";
-        $password = $this->password = "";
-        $dbName =   $this->password = "db_connection";
-
-        $conn = new mysqli($hostName,$username,$password, $dbName);
-        return $conn;
+        $connection = new mysqli($this->hostName, $this->username, $this->password, $this->dbName);
+        return $connection;
     }
 }
-$conn = new Database;
-$connect = $conn->connect();
+$database = new Database('localhost', 'root', '', 'db_connection'); //Here you have to pass your own credentials
+$connect_db = $database->connect();
 
-if (!$connect) {
+
+if (!$connect_db)
+{
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Database Connected successfully";
