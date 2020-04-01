59d5f8526a7e690a0008d5e97dccb6c1deff9f5f
faiyaz032 <faiyazrahman03@gmail.com>
2020-04-01 15:04:04 +0100

    Added a new method called disconnect()

--- faiyaz032/dbConnection/database.php
+++ faiyaz032/dbConnection/database.php
@@ -19,6 +19,11 @@ class Database{
         $connection = new mysqli($this->hostName, $this->username, $this->password, $this->dbName);
         return $connection;
     }
+
+    public function disconnect()
+    {
+            mysqli_close($this->connect()) OR die('There was a problem disconnecting from the database.');
+    }
 }
 $database = new Database('localhost', 'root', '', 'db_connection'); //Here you have to pass your own credentials
 $connect_db = $database->connect();
@@ -27,6 +32,9 @@ $connect_db = $database->connect();
 if (!$connect_db)
 {
     die("Connection failed: " . mysqli_connect_error());
+
+}elseif($connect_db){
+    echo "Database Connected successfully";
 }
-echo "Database Connected successfully";
+
 ?>
