8c24208dc0eee4ae222874ce8d27b1d0044dd7a9
Naowas Morshed Eimon <naowas.morshed.eimon@gmail.com>
2020-04-02 23:34:39 +0600

    Functions and DB

--- naowas/email/index.php
+++ naowas/email/index.php
@@ -1,3 +1,7 @@
+<?php
+include 'functions.php';
+$insert_mail=new Mysql_Connection();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -10,14 +14,37 @@
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
+<?php
+
+    if(isset($_POST['sendmail'])){
+    $mail_id = $_POST['email'];
+    $mail_sub = $_POST['subject'];
+    $mail_body = $_POST['message'];
+   
+   $sent_mail_online = mail($_POST['email'], $_POST['subject'], $_POST['message']);
+   $sql=$insert_mail->sent_mail_db($mail_id,$mail_sub,$mail_body);
+
+
+   if ($sent_mail_online && $sql ){    
+            echo "<script>alert('Mail has been sent')</script>";
+            echo"<script>window.open('index.php','_self')</script>";
+   }  
+     else {
+             die("Database connection error: " . mysqli_connect_error());
+          }
+
+    }
+
+?>
+
 <body>
     <div class="contact-clean">
-        <form method="post">
+        <form action="index.php" method="post" enctype="multipart/form-data" >
             <h2 class="text-center">Send E-mail</h2>
 			 <div class="form-group"><input class="form-control is-invalid" type="email" name="email" placeholder="Email"><small class="form-text text-danger">Please enter a correct email address.</small></div>
             <div class="form-group"><input class="form-control" type="text" name="subject" placeholder="Subject"></div>
             <div class="form-group"><textarea class="form-control" name="message" placeholder="Message" rows="14"></textarea></div>
-            <div class="form-group"><button class="btn btn-primary" type="submit">send </button></div>
+            <div class="form-group"><button class="btn btn-primary" type="submit" name="sendmail">send </button></div>
         </form>
     </div>
     <script src="assets/js/jquery.min.js"></script>
