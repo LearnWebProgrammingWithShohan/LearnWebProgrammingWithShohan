e819f25189a1b1af243125cbadf3711c3c069f5a
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2020-04-03 01:52:40 +0600

    Database initial commit

--- tawfique-to-do-app/app/classes/Database.php
+++ tawfique-to-do-app/app/classes/Database.php
@@ -3,18 +3,19 @@
 	class Database{
 
 		public static function dbconnect(){
-		$host = "localhost";
-		$user = "root";
-		$pass = "";
-		$db = "php_oop_todoapp";
-		$conn = mysqli_connect($host, $user, $pass, $db);
-			if ($conn->connect_error) {
-	    		die("Connection failed: " . $conn->connect_error);
-			}
-			// else{
-			// 	echo "Connected";
-			// }
-			return $conn;
+
+			$host = "localhost";
+			$user = "root";
+			$pass = "";
+			$db = "php_oop_todoapp";
+			$conn = mysqli_connect($host, $user, $pass, $db);
+				if ($conn->connect_error) {
+		    		die("Connection failed: " . $conn->connect_error);
+				}
+				// else{
+				// 	echo "Connected";
+				// }
+				return $conn;    	
 		}
 
 
