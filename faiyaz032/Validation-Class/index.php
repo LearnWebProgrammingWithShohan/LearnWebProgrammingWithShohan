602ad6d301d4b4ca4b3b8dd2fbdacbacef4d137d
faiyaz032 <faiyazrahman03@gmail.com>
2020-04-05 12:11:01 +0100

    Created the html form with some copied CSS

---
+++ faiyaz032/Validation-Class/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+  
+
+?>
+
+<html lang="en">
+<head>
+  <title>User Validation</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  
+  <div class="new-user">
+    <h2>Create a new user</h2>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+
+      <label>Username: </label>
+      <input type="text" name="username">
+
+      <label>Email: </label>
+      <input type="text" name="email">
+
+      <input type="submit" value="submit" name="submit">
+
+    </form>
+  </div>
+
+</body>
+</html>
