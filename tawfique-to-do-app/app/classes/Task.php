cfd69c3b82462ec0906c3ee684491902dc8abc13
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2020-04-07 00:38:47 +0600

    Database file modified for renaming table name

--- tawfique-to-do-app/app/classes/Task.php
+++ tawfique-to-do-app/app/classes/Task.php
@@ -8,7 +8,7 @@
 
             $task_name = $data;
 
-            $sql = "INSERT INTO tasks (task_name) VALUES ('$task_name')";
+            $sql = "INSERT INTO tawfiquebd_table_tasks (task_name) VALUES ('$task_name')";
             $result = mysqli_query(Database::dbconnect(), $sql);
             if ($result) {
                 $message = "Category save successfully.";
@@ -21,14 +21,14 @@
         }
 
         public function showAllTasks(){
-            $sql = "SELECT * FROM tasks ORDER BY id DESC";
+            $sql = "SELECT * FROM tawfiquebd_table_tasks ORDER BY id DESC";
             $result = mysqli_query(Database::dbconnect(), $sql);
             return $result;
         }
 
         public function updateTask($upd_taskname,$upd_id){
             
-            $sql = " UPDATE tasks SET task_name='$upd_taskname' WHERE id='$upd_id'";
+            $sql = " UPDATE tawfiquebd_table_tasks SET task_name='$upd_taskname' WHERE id='$upd_id'";
             $result = mysqli_query(Database::dbconnect(),$sql);
             if ($result) {
                 $message = "Task udpated successfully.";
@@ -42,7 +42,7 @@
         }
 
         public function deleteTask($id){
-            $sql = "DELETE FROM tasks WHERE id='$id' ";
+            $sql = "DELETE FROM tawfiquebd_table_tasks WHERE id='$id' ";
             $result = mysqli_query(Database::dbconnect(), $sql);
             return $result;
         }
