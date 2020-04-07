0c175510a3451cca9338f6fbc17ff039ae32f57a
Abdullah Al Mamun <alexmamun085@gmail.com>
2020-04-07 11:50:03 +0600

    Conditional DB connection

--- mamun724682/Authentication/includes/config.php
+++ mamun724682/Authentication/includes/config.php
@@ -5,11 +5,23 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'learnwebprogrammingwithshohan');
- 
-$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($mysqli === false){
-    die("ERROR: Could not connect. " . $mysqli->connect_error);
+
+if (DB_SERVER)
+{
+	$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    // Check connection
+	if($mysqli === false){
+		die("ERROR: Could not connect. " . $mysqli->connect_error);
+	}
 }
+elseif(PRODUCTION)
+{
+	echo 'production';
+}
+else
+{
+	echo 'Others';
+}
+
 ?>
